ef44bb8776df6a095c5190efb5dcd2d350cab252
Mateusz Popielarz <pixellos@users.noreply.github.com>
2020-07-17 08:56:48 +0200

    Update runica.ps1

--- runica.ps1
+++ runica.ps1
@@ -1,12 +1,9 @@
 $dir = "C:\Users\rogoz\Downloads\*"; 
-$f = Get-ChildItem $dir -include *.ica  | Select-Object -First 1
+$f = Get-ChildItem $dir -include *.ica | Sort-Object LastWriteTime -Descending | Select-Object -First 1
 
 
-## Get it faster
 (Get-Content $f).replace("TransportDriver=TCP/IP", "TransportDriver=UDP") | Set-Content $f
-## Get it only in one screen
-#(Get-Content $f).replace("DesktopViewer-ForceFullScreenStartup=true", "DesktopViewer-ForceFullScreenStartup=false") | Set-Content $f
-## Better skype, was working some time ago, now is not
+(Get-Content $f).replace("DesktopViewer-ForceFullScreenStartup=true", "DesktopViewer-ForceFullScreenStartup=false") | Set-Content $f
 #(Get-Content $f).replace("HDXoverUDP=Off", "HDXoverUDP=On") | Set-Content $f
 $name = $f.FullName;
 
