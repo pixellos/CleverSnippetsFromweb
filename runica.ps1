459d96536bbad27d46fba59426d9798b5ffc550e
Mateusz Popielarz <pixellos@users.noreply.github.com>
2020-09-18 10:22:26 +0200

    Update runica.ps1

--- runica.ps1
+++ runica.ps1
@@ -1,12 +1,18 @@
-$dir = "C:\Users\rogoz\Downloads\*"; 
+$dir = "$HOME\Downloads"; 
+
+while($true){
+Start-Sleep -Seconds 2
+
 $f = Get-ChildItem $dir -include *.ica | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+if($f){
 
+    (Get-Content $f).replace("TransportDriver=TCP/IP", "TransportDriver=UDP") | Set-Content $f
+    (Get-Content $f).replace("DesktopViewer-ForceFullScreenStartup=true", "DesktopViewer-ForceFullScreenStartup=false") | Set-Content $f
+    #(Get-Content $f).replace("HDXoverUDP=Off", "HDXoverUDP=On") | Set-Content $f
+    $name = $f.FullName;
 
-(Get-Content $f).replace("TransportDriver=TCP/IP", "TransportDriver=UDP") | Set-Content $f
-(Get-Content $f).replace("DesktopViewer-ForceFullScreenStartup=true", "DesktopViewer-ForceFullScreenStartup=false") | Set-Content $f
-#(Get-Content $f).replace("HDXoverUDP=Off", "HDXoverUDP=On") | Set-Content $f
-$name = $f.FullName;
-
-powershell.exe -noexit "& '$name'"
-
-Remove-Item $f
+    powershell.exe "& '$name'"
+    Start-Sleep -Seconds 2
+    Remove-Item $f
+    }
+}
